97a688af8e450710a34aa0cb7b36c58ff532e49c
ViswanadhGowd <Gowd7272@gmail.com>
2025-03-01 22:50:32 +0530

    Added the single responsibility principle.

---
+++ solid_principles/SRP.py
@@ -0,0 +1,30 @@
+"""Single-Responsibility Principle (SRP):
+The single-responsibility principle states:
+A class should have only one reason to change."""
+class Note_Book:
+    
+    def read(self)-> str:
+        return "Note_Book for read"
+    
+    def write(self)-> str:     
+        return "Note_Book for write"
+
+class Laptop:
+    
+    
+    def typeing(self)-> str:
+        return "In laptop we will code"
+    
+    def watch(self)-> str:
+        return "In laptop we will watch videos"
+
+def main():
+    note_Book=Note_Book() 
+    laptop=Laptop()
+    print(note_Book.read()) #Note_Book for read
+    print(note_Book.write())  #Note_Book for write
+    print(laptop.typeing())#In laptop we will code
+    print(laptop.watch()) #In laptop we will watch videos
+
+if __name__ == "__main__":
+    main()
