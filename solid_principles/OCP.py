ee8d3a474d9ffef395f36d7a9320f3aa17a6fa81
ViswanadhGowd <Gowd7272@gmail.com>
2025-03-02 23:36:06 +0530

    Added the OCP and LSP.

--- solid_principles/OCP.py
+++ solid_principles/OCP.py
@@ -6,44 +6,40 @@ Closed for modification means that the existing code of the class/module should 
 """
 from abc import ABC, abstractmethod
 
-class Millage(ABC):
+class Animal(ABC):
 
-    def __init__(self, vehicle: str):
-        self.vehicale=vehicle
+    def __init__(self, animal: str):
+        self.animal=animal
 
     @abstractmethod
-    def get_millage(self):
+    def get_sound(self):
         pass
 
-class Bus(Millage):
-    def __init__(self, fuel_liters):
-        self.fuel_liters=fuel_liters
-        super().__init__("BUS")
+class Dog(Animal):
+    def __init__(self):
+         super().__init__("DOG")
 
-    def get_millage(self):
-        return f"the bus filled with {self.fuel_liters} litters oil, The {self.vehicale} approximently Millage will come {self.fuel_liters * 15}"
+    def get_sound(self):
+        return f"{self.animal} Bow Bow!"
+
+class Cat(Animal):
+    def __init__(self):
+         super().__init__("Cat")
+
+    def get_sound(self):
+        return f"{self.animal} Mew Mew!"
     
-class Bike(Millage):
-    def __init__(self, fuel_liters):
-        self.fuel_liters=fuel_liters
-        super().__init__("Bike")
-
-    def get_millage(self):
-        return f"the Bike filled with {self.fuel_liters} litters oil, The {self.vehicale} approximently Millage will come {self.fuel_liters * 65}"
-
+ 
 def main():
-    vehicle_list=[Bus(10), Bike(5)]
-    def fetch_data(vehicle_list):
-        for each in vehicle_list:
-            print(each.get_millage())
-    fetch_data(vehicle_list)
+    print(Cat().get_sound()) #Cat Mew Mew!
+    print(Dog().get_sound()) #DOG Bow Bow!
 
 if __name__ == "__main__":
     main()
 
 """
 Output :
+Cat Mew Mew!
+DOG Bow Bow!
 
-the bus filled with 10 litters oil, The BUS approximently Millage will come 150
-the Bike filled with 5 litters oil, The Bike approximently Millage will come 325
 """
